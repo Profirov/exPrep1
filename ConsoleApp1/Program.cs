c02f68be798abeda6893758b461270db3a58572b
Bozh <bojidarprofirov@abv.bg>
2024-02-14 21:27:40 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialEnergy = int.Parse(Console.ReadLine());
+            string Line = "";
+            int wins = 0;
+
+            while ((Line = Console.ReadLine()) != "End of battle")
+            {
+                int distance = int.Parse(Line);
+
+                if (distance > initialEnergy)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {wins} won battles and {initialEnergy} energy");
+                    return;
+                }
+
+                initialEnergy -= distance;
+                wins++;
+                if (wins % 3 == 0)
+                {
+                    initialEnergy += wins;
+                }
+                
+            }
+            Console.WriteLine($"Won battles: {wins}. Energy left: {initialEnergy}");
+        }
+    }
+}
---
+++ examPrep2/Program.cs
@@ -0,0 +1,65 @@
+﻿
+
+List<string> loot = Console.ReadLine().Split("|").ToList();
+
+string Line = "";
+while ((Line = Console.ReadLine()) != "Yohoho!")
+{
+    string[] tokens = Line.Split();
+    string command = tokens[0];
+    if (command == "Loot")
+    {
+        for (int i = 1; i < tokens.Length; i++)
+        {
+            if (!loot.Contains(tokens[i]))
+            {
+                loot.Insert(0, tokens[i]);
+            }
+        }
+    }
+    else if (command == "Drop")
+    {
+        int index = int.Parse(tokens[1]);
+        if (index >= 0 && index < loot.Count)
+        {
+            string removedItem = loot[index];
+            loot.RemoveAt(index);
+            loot.Add(removedItem);
+        }
+    }
+    else
+    {
+        int itemsCount = int.Parse(tokens[1]);
+        List<string> removeedItems = new List<string>();
+        for (int i = 0; i < itemsCount; i++)
+        {
+            if (loot.Count != 0)
+            {
+                
+                string removedItem = loot[loot.Count - 1];
+                loot.RemoveAt(loot.Count - 1);
+                removeedItems.Add(removedItem);
+            }
+        }
+        if (removeedItems.Count != 0)
+        {
+            removeedItems.Reverse();
+            Console.WriteLine(string.Join(", ", removeedItems));
+        }
+    }
+}
+if (loot.Count == 0)
+{
+    Console.WriteLine("Failed treasure hunt.");
+    return;
+}
+double sum = 0;
+foreach (string item in loot)
+{ 
+   sum += item.Length;
+}
+double averageGain = sum / loot.Count;
+Console.WriteLine($"Average treasure gain: {averageGain:f2} pirate credits.");
+
+
+private static void CounterStrike()..
